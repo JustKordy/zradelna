5b69cd6c3b8c7936c6d01ee692c9dcffef289ab9
JustKordy <honajsek@seznam.cz>
2025-01-29 13:59:02 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const post = await db.query.posts.findFirst();
+
   return (
     <>
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">

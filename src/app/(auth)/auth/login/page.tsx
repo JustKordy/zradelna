889ba7c1372efe5d587ee12441f9ae0bb3807ec3
JustKordy <honajsek@seznam.cz>
2025-01-29 18:36:50 +0100

    proste push

---
+++ src/app/(auth)/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import {Avatar, AvatarGroup, AvatarIcon} from "@heroui/avatar";
+
+export default function Login(){
+    return (
+        <>
+            <div className="w-screen h-screen bg-zinc-900 flex justify-center items-center">
+                <div className="w-1/2">
+                    
+                </div>
+            </div>
+        </>
+    )
+}

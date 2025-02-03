cca6eebe60818ec21128f9ba5b7e858149b1566c
Matyáš Bár <matyas.barr@gmail.com>
2025-02-03 11:17:54 +0100

    feat(db): Change the db schema (again)

--- src/app/(test)/test_a/actions.ts
+++ src/app/(test)/test_a/actions.ts
@@ -1,15 +1,10 @@
 "use server";
 
 import { getMenu } from "~/server/queries/menus";
-import { makeUserChoice } from "~/server/queries/user";
-import type { MenuChoiceWeek } from "~/types/menuChoices";
 
 export async function lala() {
   console.log("LALA");
-  const options: MenuChoiceWeek = [1, 1, null, null, 2];
-  // const data = await createMenu(getLastMonday(), options);
-  const week = await getMenu();
-  if (!week) throw new Error(":(");
-  const data = await makeUserChoice(week.id, options);
-  console.log(data);
+  const week = await getMenu(new Date());
+  console.log(week);
+  console.log(week?.menusToDIshes[0]?.dishes.name);
 }

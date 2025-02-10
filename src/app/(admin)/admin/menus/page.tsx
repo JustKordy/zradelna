0d86f5cc6b91db34cce0ffe04fecb6eb93cddd1e
HSko <103035284+HSko27@users.noreply.github.com>
2025-02-10 16:25:04 +0100

    přidání výběr jídel

--- src/app/(admin)/admin/menus/page.tsx
+++ src/app/(admin)/admin/menus/page.tsx
@@ -1,30 +1,93 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 
-//text-slate-50 font-bold p-4 bg-orange-400 px-7 hover:bg-orange-500 ease-in-out duration-300 rounded-xl
-
 function Page() {
-    const [page, setPage] = useState<number>();
-    
-    const handleClick = (pageNumber: number) => {
-        setPage(pageNumber);
-    }
+  const [page, setPage] = useState<number>(1);
+
+  const handleClick = (pageNumber: number) => {
+    setPage(pageNumber);
+  };
 
   return (
     <div className="">
-      <div className="grid  grid-cols-5 p-5 grid-rows-5 gap-4">
-        <div className="col-span-5 bg-slate-100 rounded-lg row-span-2">
-            <div className="flex flex-row p-4 gap-4 justify-center">
-                <button onClick={() => handleClick(1)} className={`${page === 1 ? "bg-orange-400" : "bg-slate-50"} text-black border-2 border-orange-400 hover:text-slate-50 hover:border-orange-500 font-bold p-4 px-7 hover:bg-orange-500 ease-in-out duration-300 rounded-lg`}>PO</button>
-                <button onClick={() => handleClick(2)} className={`${page === 2 ? "bg-orange-400" : "bg-slate-50"} text-black border-2 border-orange-400 hover:text-slate-50 hover:border-orange-500 font-bold p-4 px-7 hover:bg-orange-500 ease-in-out duration-300 rounded-lg`}>ÚT</button>
-                <button onClick={() => handleClick(3)} className={`${page === 3 ? "bg-orange-400" : "bg-slate-50"} text-black border-2 border-orange-400 hover:text-slate-50 hover:border-orange-500 font-bold p-4 px-7 hover:bg-orange-500 ease-in-out duration-300 rounded-lg`}>ST</button>
-                <button onClick={() => handleClick(4)} className={`${page === 4 ? "bg-orange-400" : "bg-slate-50"} text-black border-2 border-orange-400 hover:text-slate-50 hover:border-orange-500 font-bold p-4 px-7 hover:bg-orange-500 ease-in-out duration-300 rounded-lg`}>ČT</button>
-                <button onClick={() => handleClick(5)} className={`${page === 5 ? "bg-orange-400" : "bg-slate-50"} text-black border-2 border-orange-400 hover:text-slate-50 hover:border-orange-500 font-bold p-4 px-7 hover:bg-orange-500 ease-in-out duration-300 rounded-lg`}>PÁ</button>
-            </div>
+      <div className=" w-full gap-4 p-5">
+        <div className="w-full rounded-lg bg-slate-100">
+          <div className="flex w-full flex-row justify-center gap-4 p-4">
+            <button
+              onClick={() => handleClick(1)}
+              className={`${page === 1 ? "bg-orange-500 text-white" : "bg-slate-50"} rounded-lg border-2 border-orange-500 p-4 px-7 font-bold text-orange-500 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50`}
+            >
+              PO
+            </button>
+            <button
+              onClick={() => handleClick(2)}
+              className={`${page === 2 ? "bg-orange-500 text-white" : "bg-slate-50"} rounded-lg border-2 border-orange-500 p-4 px-7 font-bold text-orange-500 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50`}
+            >
+              ÚT
+            </button>
+            <button
+              onClick={() => handleClick(3)}
+              className={`${page === 3 ? "bg-orange-500 text-white" : "bg-slate-50"} rounded-lg border-2 border-orange-500 p-4 px-7 font-bold text-orange-500 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50`}
+            >
+              ST
+            </button>
+            <button
+              onClick={() => handleClick(4)}
+              className={`${page === 4 ? "bg-orange-500 text-white" : "bg-slate-50"} rounded-lg border-2 border-orange-500 p-4 px-7 font-bold text-orange-500 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50`}
+            >
+              ČT
+            </button>
+            <button
+              onClick={() => handleClick(5)}
+              className={`${page === 5 ? "bg-orange-500 text-white" : "bg-slate-50"} rounded-lg border-2 border-orange-500 p-4 px-7 font-bold text-orange-500 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50`}
+            >
+              PÁ
+            </button>
+          </div>
         </div>
-        <div className="col-span-2 bg-slate-100 row-span-3 row-start-3">5</div>
-        <div className="col-span-2 bg-slate-100 col-start-3 row-span-3 row-start-3">6</div>
-        <div className="col-start-5 bg-slate-100 row-span-3 row-start-3">7</div>
+        <div className="flex w-full flex-col md:flex-row justify-center gap-4 mt-10">
+          <div className=" w-full flex-col md:w-1/2 flex justify-center gap-4 text-center bg-slate-100">
+            <form action="">
+              <h1 className="my-5 font-bold text-orange-500 text-lg">Přidání menu</h1>
+              <label htmlFor="jidlo1" className="font-bold text-orange-500 mx-10">Jídlo 1:</label>
+              <input name="jidlo1" className="rounded-lg border-2 border-orange-400 py-2 me-2 focus:outline-2 focus:outline-offset-2 focus:outline-orange-400 ps-1 placeholder:text-orange-400" placeholder="Hledat Jídlo" type="text" />
+              <button className="rounded-lg bg-white border-2 border-orange-400 px-5 py-2 font-bold text-orange-400 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50 ">Potvrdit</button>
+              <div className="flex flex-col">
+                <h1 className="my-2">vybrané jídlo</h1>
+              </div>
+            </form>
+            <form action="">
+              <label htmlFor="jidlo1" className="font-bold text-orange-500 mx-10">Jídlo 2:</label>
+              <input name="jidlo1" className="rounded-lg border-2 border-orange-400 py-2 me-2 focus:outline-2 focus:outline-offset-2 focus:outline-orange-400 ps-1 placeholder:text-orange-400" placeholder="Hledat Jídlo" type="text" />
+              <button className="rounded-lg bg-white border-2 border-orange-400 px-5 py-2 font-bold text-orange-400 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50 ">Potvrdit</button>
+              <div className="flex flex-col">
+                <h1 className="my-2">vybrané jídlo</h1>
+              </div>
+            </form>
+            <form action="">
+              <label htmlFor="jidlo1" className="font-bold text-orange-500 mx-10">Jídlo 3:</label>
+              <input name="jidlo1" className="rounded-lg border-2 border-orange-400 py-2 me-2 focus:outline-2 focus:outline-offset-2 focus:outline-orange-400 ps-1 placeholder:text-orange-400" placeholder="Hledat Jídlo" type="text" />
+              <button className="rounded-lg bg-white border-2 border-orange-400 px-5 py-2 font-bold text-orange-400 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50 ">Potvrdit</button>
+              <div className="flex flex-col">
+                <h1 className="my-2">vybrané jídlo</h1>
+              </div>
+            </form>
+            <form action="">
+              <label htmlFor="jidlo1" className="font-bold text-orange-500 mx-8">Polévka: </label>
+              <input name="jidlo1" className="rounded-lg border-2 border-orange-400 py-2 me-2 focus:outline-2 focus:outline-offset-2 focus:outline-orange-400 ps-1 placeholder:text-orange-400" placeholder="Hledat Polévku" type="text" />
+              <button className="rounded-lg bg-white border-2 border-orange-400 px-5 py-2 font-bold text-orange-400 duration-300 ease-in-out hover:border-orange-400 hover:bg-orange-400 hover:text-slate-50 ">Potvrdit</button>
+              <div className="flex flex-col">
+                <h1 className="my-2">vybrané jídlo</h1>
+              </div>
+            </form>
+          </div>
+          <div className=" w-full md:w-1/2 flex justify-center text-center bg-slate-100">
+            <form action="">
+              <h1>Přidání menu</h1>
+              <input type="text" />
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );

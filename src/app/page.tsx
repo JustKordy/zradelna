ddefa200a53cc62ddee88afaa6be18eb917374f7
JustKordy <honajsek@seznam.cz>
2025-02-19 10:59:34 +0100

    zmeny

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@
 import { redirect } from "next/navigation";
 import { HomepageComponent } from "~/comps/HomepageComponent";
 import type IWeek from "~/interfaces/IWeek";
-import { getMenu } from "~/server/queries/menus";
+import { getMenu, getMenusInRange } from "~/server/queries/menus";
 import { getUser } from "~/server/queries/user";
+
 
 export default async function HomePage() {
   const user = await getUser()
 
   if(!user) redirect("/auth/login")
-
-
+  
+  // const menus = await getMenusInRange(new Date(), new Date())
  
   return (
     <HomepageComponent user={user} />

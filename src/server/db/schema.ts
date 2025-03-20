3f3eb09b40a129f448a92cea82d283f4f0af3b4e
HSko <103035284+HSko27@users.noreply.github.com>
2025-03-20 10:24:44 +0100

    rsvrs

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,19 +3,14 @@
 
 import { relations, sql } from "drizzle-orm";
 import {
-  boolean,
   date,
   index,
   integer,
   pgSchema,
   pgTableCreator,
-  primaryKey,
-  text,
   timestamp,
-  uniqueIndex,
   varchar,
 } from "drizzle-orm/pg-core";
-
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
  * database instance for multiple projects.
@@ -26,37 +21,13 @@ export const appSchema = pgSchema("app");
 
 export const createTable = pgTableCreator((name) => `zradelna_${name}`);
 
-export const dishes = createTable(
-  "dishes",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 255 }).unique().notNull(),
-    isSoup: boolean("is_soup").default(false).notNull(),
-    description: text("description"),
-    imgURL: varchar("img_url", { length: 1024 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull()
-      .$onUpdate(() => new Date()),
-  },
-  (table) => [
-    index("name_idx").on(table.name),
-    index("is_soup_idx").on(table.isSoup),
-  ],
-);
-
-// Menu for a day
 export const menus = createTable(
   "menus",
   {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
     date: date("date", { mode: "date" }).unique().notNull(),
-    soupId: integer("soup_id")
-      .references(() => dishes.id)
-      .notNull(),
+    soup: varchar("soup", { length: 255 }).notNull(),
+    dishes: varchar("dishes", { length: 255 }).array().notNull(),
 
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
@@ -69,80 +40,31 @@ export const menus = createTable(
   (table) => [index("date_idx").on(table.date)],
 );
 
-// Define the join table for menus and dishes
-export const menuDishes = createTable(
-  "menu_dishes",
-  {
-    menuId: integer("menu_id")
-      .notNull()
-      .references(() => menus.id),
-    dishId: integer("dish_id")
-      .notNull()
-      .references(() => dishes.id),
-  },
-  (table) => [primaryKey({ columns: [table.menuId, table.dishId] })],
-);
-
-export const menusR = relations(menus, ({ many, one }) => ({
-  menusToDishes: many(menuDishes),
-  userChoices: many(userChoices),
-  soup: one(dishes, {
-    fields: [menus.soupId],
-    references: [dishes.id],
-  }),
-}));
-export const dishesR = relations(dishes, ({ many }) => ({
-  dishesToMenus: many(menuDishes),
-  userChoices: many(userChoices),
-  soups: many(menus),
-}));
-export const menuDishesR = relations(menuDishes, ({ one }) => ({
-  menus: one(menus, {
-    fields: [menuDishes.menuId],
-    references: [menus.id],
-  }),
-  dishes: one(dishes, {
-    fields: [menuDishes.dishId],
-    references: [dishes.id],
-  }),
-}));
-
-// User's choice
-export const userChoices = createTable(
-  "user_choice",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    userId: varchar("user_id", { length: 255 }).notNull(),
-    menuId: integer("menu_id")
-      .references(() => menus.id)
-      .notNull(),
-    dishId: integer("dish_id")
-      .references(() => dishes.id)
-      .notNull(),
-    toGo: boolean("to_go").default(false).notNull(),
-
-    createdAt: timestamp("created_at")
+export const userChoices = createTable("user_choices", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  userId: varchar("user_id", {length: 128}).notNull(),
+  menuId: integer("menu_id").references(() => menus.id).notNull(),
+  dish: varchar("dish", {length: 255}).notNull(),
+  amount: integer("amount").default(1).notNull(),
+  
+  createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull()
       .$onUpdate(() => new Date()),
-  },
-  (table) => [
-    index("user_id_idx").on(table.userId),
-    index("menu_id_idx").on(table.menuId),
-    uniqueIndex("user_id_to_menu_id_idx").on(table.userId, table.menuId),
-  ],
-);
 
-export const userChoiceR = relations(userChoices, ({ one }) => ({
-  dish: one(dishes, {
-    fields: [userChoices.dishId],
-    references: [dishes.id],
-  }),
-  menu: one(menus, {
+}, (table) => [index("user_id_idx").on(table.userId), index("menu_id_idx").on(table.menuId)] )
+
+
+export const userChoicesR = relations(userChoices, ({one}) => ({
+  userChoicesToMenus: one(menus, {
     fields: [userChoices.menuId],
     references: [menus.id],
-  }),
-}));
+  })
+}))
+
+export const menusR = relations(userChoices, ({many}) => ({
+  userChoicesToMenus: many(userChoices)
+}))

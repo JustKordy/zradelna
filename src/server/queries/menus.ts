14bb15b629ddebb43211e895db86b1e1b8c6abc6
Matyáš Bár <matyas.barr@gmail.com>
2025-03-20 18:18:31 +0100

    refactor: Menu queries

--- src/server/queries/menus.ts
+++ src/server/queries/menus.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "~/server/db";
-import { menuDishes, menus, userChoices } from "~/server/db/schema";
+import { menus } from "~/server/db/schema";
 import { between, eq } from "drizzle-orm";
 import { getUser } from "./user";
 
@@ -12,23 +12,10 @@ export async function getMenu(date: Date) {
   });
 }
 
-// Get count of user choices per mennu
-export async function getUserChoicesCount(date: Date) {
-  return 5;
-}
-
 // Gets all menus in range
 export async function getMenusInRange(from: Date, to: Date) {
   return db.query.menus.findMany({
     where: between(menus.date, from, to),
-    with: {
-      soup: true,
-      menusToDishes: {
-        with: {
-          dishes: true,
-        },
-      },
-    },
   });
 }
 
@@ -40,36 +27,24 @@ export async function getMenusInRangeWithUserSelect(from: Date, to: Date) {
   return db.query.menus.findMany({
     where: between(menus.date, from, to),
     with: {
-      userChoices: {
-        where: eq(userChoices.userId, user.id),
-      },
-      soup: true,
-      menusToDishes: {
-        with: {
-          dishes: true,
-        },
-      },
+      menusToUserChoices: true,
     },
   });
 }
 
 // CREATE
-export async function createMenu(date: Date = new Date(), soupId: number) {
+export async function createMenu(
+  date: Date = new Date(),
+  soup: string,
+  dishes: string[],
+) {
   console.log(
     "[INFO][DB]: Creating a new menu: ",
     date.toISOString().split("T")[0],
   );
   return db.insert(menus).values({
     date,
-    soupId,
+    soup,
+    dishes,
   });
-}
-
-export async function addMenuItems(menuId: number, dishIds: number[]) {
-  console.log(
-    `[INFO][DB]: Creating a new menu item: menuId: ${menuId}: dishId: ${JSON.stringify(dishIds)}`,
-  );
-  const batch = dishIds.map((x) => ({ menuId, dishId: x }));
-
-  return db.insert(menuDishes).values(batch);
 }
